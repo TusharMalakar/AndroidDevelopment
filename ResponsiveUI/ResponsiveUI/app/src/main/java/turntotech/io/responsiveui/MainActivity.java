62f89ca6986c6734db5fc2d8751efc8223618bb9
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-03-26 19:21:15 -0400

    commit

---
+++ ResponsiveUI/ResponsiveUI/app/src/main/java/turntotech/io/responsiveui/MainActivity.java
@@ -0,0 +1,13 @@
+package turntotech.io.responsiveui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
